715c64c43d94b3c9d9a7d45320dc34afedebccaa
Mertcan Elçi <mertcanelci@outlook.com>
2022-02-28 14:03:46 +0300

    Project files created

---
+++ src/test/java/com/softtech/webapp/Case3AyroVApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softtech.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Case3AyroVApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
